6161e2e6a27c6824cc2ca1070c0c4649d18ff174
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-07-11 16:40:20 -0400

    Added a test for milex-initialize

---
+++ tests/integration/test_milex_initialize.py
@@ -0,0 +1,26 @@
+from argparse import Namespace
+from unittest.mock import patch
+from milex_scheduler.apps.milex_initialize import main
+import pytest
+import os
+
+
+@pytest.fixture
+def mock_parse_known_args():
+    with patch("argparse.ArgumentParser.parse_args") as mock_parse_known_args:
+        mock_parse_known_args.return_value = Namespace(bundle_name="bundle_name")
+        yield mock_parse_known_args
+
+
+@pytest.fixture
+def mock_load_config(tmp_path):
+    mock_config = {"local": {"path": tmp_path}}
+    os.makedirs(tmp_path / "jobs", exist_ok=True)
+    with patch(
+        "milex_scheduler.save_load_jobs.load_config", return_value=mock_config
+    ) as mock_load_config:
+        yield mock_load_config
+
+
+def test_main(mock_parse_known_args, mock_load_config):
+    main()
