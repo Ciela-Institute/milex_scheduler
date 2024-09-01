0570356cd269c0bfa4a7826655fe65b6e5936c6a
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-09-01 00:18:58 -0400

    Small refactoring of milex-initialize to be consistent with milex-schedule

--- src/milex_scheduler/apps/milex_initialize.py
+++ src/milex_scheduler/apps/milex_initialize.py
@@ -5,7 +5,7 @@ from ..save_load_jobs import save_bundle
 def parse_args():
     # fmt: off
     parser = argparse.ArgumentParser(description='Initialize a bundle of jobs to run with SLURM')
-    parser.add_argument('bundle', help='Name of the job bundle (JSON file containing multiple jobs/scripts to be scheduled).')
+    parser.add_argument('name', help='Name of the job bundle (JSON file containing multiple jobs/scripts to be scheduled).')
     # fmt: on
     args = parser.parse_args()
     return args
@@ -13,4 +13,4 @@ def parse_args():
 
 def main():
     args = parse_args()
-    save_bundle({}, args.bundle)
+    save_bundle({}, args.name)
